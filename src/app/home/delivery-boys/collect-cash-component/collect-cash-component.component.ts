c464e34589bad026a9e33a0059944e8704220075
akhilashok7539 <akhilashok7539@gmail.com>
2021-04-01 00:09:14 -0700

    changes

--- src/app/home/delivery-boys/collect-cash-component/collect-cash-component.component.ts
+++ src/app/home/delivery-boys/collect-cash-component/collect-cash-component.component.ts
@@ -13,11 +13,16 @@ export class CollectCashComponentComponent implements OnInit {
   apiUrl;
   id;
   points;
+  cashinhand;
+  earningspointdisplay;
   constructor(@Inject(MAT_DIALOG_DATA) data, private easydeelservice:EasydealService,
   private toaster:ToastrService,
   private dialogRef: MatDialogRef<CollectCashComponentComponent>) 
   { 
     this.id = data['_id'];
+    console.log(data);
+    this.cashinhand = data['money_hand'];
+    this.earningspointdisplay = data['earnings'];
 
   }
   ngOnInit() {
